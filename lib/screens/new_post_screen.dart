8ae6a55c09d346d1277ed7bd74cea565de693bae
kallytang <tang.kally@gmail.com>
2021-03-07 11:02:32 -0800

    adding location data and camera feature to new post screen

--- lib/screens/new_post_screen.dart
+++ lib/screens/new_post_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:location/location.dart';
+// import 'package:share/share.dart';
 
 class NewPostScreen extends StatefulWidget {
   static const routeName = "newPost";
@@ -8,11 +12,31 @@ class NewPostScreen extends StatefulWidget {
 }
 
 class _NewPostScreenState extends State<NewPostScreen> {
+  File imageFile;
+  LocationData locationData;
+  final picker = ImagePicker();
+
+  void initState() {
+    super.initState();
+    retrieveLocation();
+  }
+  void getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    imageFile = File(pickedFile.path);
+    setState(() {});
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Create Post")),
+      
 
     );
   }
+  void retrieveLocation() async {
+    var locationService = Location();
+    locationData = await locationService.getLocation();
+    setState(() {});
+  }
 }
+
