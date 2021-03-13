ee22e01cd39ffc36ebc7406a514226125eda4bdf
kallytang <tang.kally@gmail.com>
2021-03-13 14:40:57 -0800

    added features to new post screen and added a dto mode

--- lib/screens/new_post_screen.dart
+++ lib/screens/new_post_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:location/location.dart';
@@ -7,6 +8,7 @@ import 'package:location/location.dart';
 
 class NewPostScreen extends StatefulWidget {
   static const routeName = "newPost";
+
   @override
   _NewPostScreenState createState() => _NewPostScreenState();
 }
@@ -15,28 +17,115 @@ class _NewPostScreenState extends State<NewPostScreen> {
   File imageFile;
   LocationData locationData;
   final picker = ImagePicker();
+  final formKey = GlobalKey<FormState>();
 
   void initState() {
     super.initState();
     retrieveLocation();
   }
+
   void getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.camera);
     imageFile = File(pickedFile.path);
     setState(() {});
   }
+
+  void getGalleryImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    imageFile = File(pickedFile.path);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Create Post")),
-      
-
+      body: SingleChildScrollView(
+        child: Padding(
+          child: postForm(formKey),
+          padding: const EdgeInsets.all(20),
+        ),
+      ),
     );
   }
+
   void retrieveLocation() async {
     var locationService = Location();
     locationData = await locationService.getLocation();
     setState(() {});
   }
+
+  Widget getCameraButton() {
+    return Container(
+        child: ElevatedButton(
+            onPressed: () {
+              getImage();
+            },
+            child: Text("Take Photo")));
+  }
+
+  Widget getGalleryButton() {
+    return Container(
+        child: ElevatedButton(
+            onPressed: () {
+              getGalleryImage();
+            },
+            child: Text("Get Photo From Gallery")));
+  }
+
+  Widget postForm(GlobalKey<FormState> formKey) {
+    return Form(
+      key: formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [imageManager(), numberWastedField(), postButton()],
+      ),
+    );
+  }
+
+  Widget imageManager() {
+    if (imageFile == null) {
+      return Column(
+        children: [getCameraButton(), getGalleryButton()],
+      );
+    } else {
+      return imageHolder();
+    }
+  }
+
+  Widget imageHolder() {
+    return Container(
+      child: Image.file(imageFile),
+    );
+  }
 }
 
+Widget numberWastedField() {
+  return Padding(
+    padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+    child: TextFormField(
+        autofocus: true,
+        keyboardType: TextInputType.number,
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.digitsOnly
+        ],
+        decoration: InputDecoration(
+            labelText: 'Number Wasted', border: OutlineInputBorder()),
+        onSaved: (value) {
+          //  add data here
+        },
+        validator: (value) {
+          if (value.isEmpty) {
+            return "Please Enter a Rating";
+          } else {
+            return null;
+          }
+        }),
+  );
+}
+
+Widget postButton() {
+  return Container(
+    color: Colors.blue,
+  );
+}
